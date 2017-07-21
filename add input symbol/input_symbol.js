bb28d736e98d638c0fbe7e13b25ed8c4b4f92d1a
Łukasz Komajda <pukie@users.noreply.github.com>
2017-07-21 11:48:40 +0200

    Update input_symbol.js
    fixed cursor position

--- add input symbol/input_symbol.js
+++ add input symbol/input_symbol.js
@@ -1,31 +1,32 @@
 .directive('symbolInput', function($browser) {
-    return {
-        require: 'ngModel',
-        priority: 1,
-        link: function($scope, $element, $attrs, ngModelCtrl) {
-            var listener = function() {
-                $element.val($element.val().replace(/[^0-9]/g, '')+' '+$attrs.symbol);
-            };
+        return {
+            require: 'ngModel',
+            //priority: 1,
+            link: function($scope, $element, $attrs, ngModelCtrl) {
 
-            // This runs when we update the text field
-            ngModelCtrl.$parsers.push(function(viewValue) {
-              var value = viewValue.replace(/[^0-9]/g, '');
-                return value
-            });
-            ngModelCtrl.$render = function() {
-                $element.val(ngModelCtrl.$viewValue+' '+$attrs.symbol);
-            };
-            $element.bind('change', listener);
-            $element.bind('keydown', function(e) {
-  						 if (e.which === 8 && e.target.nodeName === "INPUT") { 
-               $element.val($element.val().replace(/[^0-9]/g, ''));
-      				}
-                $browser.defer(listener); 
-            });
-
-        }
-
-    };
+                var listener = function() {
+                 var el = $element[0];
+                 var cursorPos = el.selectionStart;
+                     $element.val($element.val().replace(/[^0-9.]/g, '')+' '+$attrs.symbol);
+                     cursorPos == $element.val().length ?
+                     	el.setSelectionRange($element.val().length, $element.val().length-$attrs.symbol.length-1)
+                     :
+                     	el.setSelectionRange($element.val().length, cursorPos);
+                };
+                ngModelCtrl.$parsers.push(function(viewValue) {
+                    var value = viewValue.replace(/[^0-9.]/g, '');
+                    return $attrs.symbol=='%' ? value/100 : value ;
+                });
+                ngModelCtrl.$render = function() {
+                    var value = $attrs.symbol=='%' ? ngModelCtrl.$viewValue*100 || 0: ngModelCtrl.$viewValue || 0;
+                    $element.val(value+' '+$attrs.symbol);
+                };
+                $element.bind('change', listener);
+                $element.bind('keydown', function(e) {
+                    $browser.defer(listener);
+                });
+            }
+        };
 });
 
 
