bf681378672b5aa27e8ea10b590c0be776be8d45
Łukasz Komajda <pukie@users.noreply.github.com>
2017-08-11 11:30:23 +0200

    Update separator+add symbol.js

--- separator+add symbol/separator+add symbol.js
+++ separator+add symbol/separator+add symbol.js
@@ -3,47 +3,60 @@ var fessmodule = angular.module('myModule', []);
 fessmodule.controller('fessCntrl', function ($scope) {
     $scope.test = 0;
 
+}) var fessmodule = angular.module('myModule', []);
+
+fessmodule.controller('fessCntrl', function ($scope) {
+    $scope.test = 3.232323;
+
 }) .directive('symbolInput', function($browser) {
         return {
-            require: 'ngModel',
-            priority: 1,
-            link: function($scope, $element, $attrs, ngModelCtrl) {
+            	require: 'ngModel',
+           	 //priority: 1,
+		link: function($scope, $element, $attrs, ngModelCtrl) {
 
                 var listener = function() {
-                console.log($element.val())
-               	
-                 var el = $element[0];
-                 
-                 var cursorPos = el.selectionStart;
-                 var stringLength = $element.val().length;
-                
-                     $element.val($element.val().replace(/[^0-9.]/g, '')
-                     .replace(/\B(?=(\d{3})+(?!\d))/g, " ")+' '+$attrs.symbol);
-                       var cursorPos = el.selectionStart;
+
+
+                    var el = $element[0];
+                    var value=$element.val().replace(/[^0-9.]/g, '');
+                    var index  = value.indexOf('.');
+
+                    if((value).substr(index).length>3){
+                        value = parseFloat(value).toFixed(2);
+                    }
+
+                    $element.val(value.replace(/\B(?=(\d{3})+(?!\d))/g, " ")+' '+$attrs.symbol);
+                    var cursorPos = el.selectionStart;
                     var countSeparator =  ($element.val().substr(0, cursorPos).match(/\s\d/g) || []).length;
-             
-                     cursorPos+countSeparator > $element.val().length-$attrs.symbol.length-1+countSeparator ?
-                     	el.setSelectionRange($element.val().length, $element.val().length-$attrs.symbol.length-1)
-                     :
-                     	el.setSelectionRange($element.val().length, cursorPos+countSeparator);
+                    var newCursorPos = cursorPos+countSeparator;
+                    /* jshint ignore:start */
+                    newCursorPos > $element.val().length-$attrs.symbol.length-1+countSeparator ?
+                        el.setSelectionRange($element.val().length, $element.val().length-$attrs.symbol.length-1) :
+                        el.setSelectionRange($element.val().length, newCursorPos);
+                    /* jshint ignore:end */
                 };
                 ngModelCtrl.$parsers.push(function(viewValue) {
                     var value = viewValue.replace(/[^0-9.]/g, '');
-                    return $attrs.symbol=='%' ? value/100 : value ;
+                    return parseFloat($attrs.symbol=='%' ? value/100 : value).toFixed(4) ;
                 });
                 ngModelCtrl.$render = function() {
-                    var value = $attrs.symbol=='%' ? ngModelCtrl.$viewValue*100 || 0: ngModelCtrl.$viewValue || 0;
-                    $element.val(value+' '+$attrs.symbol);
+
+                    var value = ($attrs.symbol=='%' ? ngModelCtrl.$viewValue*100 || 0: ngModelCtrl.$viewValue || 0).toString();
+
+                    if(value.indexOf('.') > 0){
+                        value=(parseFloat(value)).toFixed(2);
+                    }
+                    $element.val(value.replace(/\B(?=(\d{3})+(?!\d))/g, " ")+' '+$attrs.symbol);
                 };
                 $element.bind('change', listener);
                 $element.bind('keydown', function(e) {
-                		 if (e.which !== 8 && e.target.nodeName === "INPUT") { 
-                 $browser.defer(listener);
-       				}
-                  
+                    if (e.which !== 8 && e.target.nodeName === "INPUT") {
+                        $browser.defer(listener);
+                    }
+
                 });
             }
         };
     });
 	
-	//http://jsfiddle.net/pukie/SAWsA/9684/
+	//http://jsfiddle.net/SAWsA/9752/
